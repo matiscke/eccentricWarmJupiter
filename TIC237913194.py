7a998aeb50155dfdf04e6125c831fb6801822cde
Martin Schlecker <schlecker@mpia.de>
2020-03-16 17:15:17 +0100

    load results directly from file for plots

--- TIC237913194.py
+++ TIC237913194.py
@@ -10,8 +10,9 @@ except ModuleNotFoundError:
     print('module "popsyntools" not found. Skipping plot styles therein.')
 
 datafolder = 'data/'
+out_folder = 'out/16_tess+noGP'
 # out_folder = 'out/17_tess+GP'
-out_folder = 'out/18_tess+chat+GP'
+# out_folder = 'out/18_tess+chat+GP'
 # out_folder = 'out/19_tess+chat+feros+noGP'
 # out_folder = 'out/20_tess+chat+feros+GP'
 # out_folder = 'out/21_tess+chat+feros+CORALIE+noGP'
@@ -27,11 +28,11 @@ if 'GP' in out_folder and not 'noGP' in out_folder:
 else:
     GP = False
 
-# instruments_lc = ['TESSERACT+TESS']
-instruments_lc = ['TESSERACT+TESS', 'CHAT+i']
+instruments_lc = ['TESSERACT+TESS']
+# instruments_lc = ['TESSERACT+TESS', 'CHAT+i']
 outlierIndices = [992, 1023, 1036, 1059, 1060, 1061, 1078, 1082, 1083, 1084, 1602]
-# instruments_rv = ['FEROS']
-instruments_rv = ['FEROS', 'CORALIE']
+instruments_rv = ['FEROS']
+# instruments_rv = ['FEROS', 'CORALIE']
 colors_rv = ['orangered', 'cornflowerblue', 'purple', 'forestgreen']
 
 # Stellar parameters for TIC237913194
@@ -67,22 +68,22 @@ def get_priors(GP=True):
     'GP_timescale_TESSERACT+TESS' : ['loguniform', [1e-4, 2]],
 
     # # CHAT+i
-    'q1_CHAT+i' : ['uniform', [0., 1.]],
-    ##### 'q2_CHAT+i' : ['uniform', [0., 1.]],
-    'sigma_w_CHAT+i' : ['loguniform', [1e-5,1e5]],
-    'mflux_CHAT+i' : ['normal', [0.0,0.1]],
-    'mdilution_CHAT+i' : ['fixed', 1.0],
+    # 'q1_CHAT+i' : ['uniform', [0., 1.]],
+    # ##### 'q2_CHAT+i' : ['uniform', [0., 1.]],
+    # 'sigma_w_CHAT+i' : ['loguniform', [1e-5,1e5]],
+    # 'mflux_CHAT+i' : ['normal', [0.0,0.1]],
+    # 'mdilution_CHAT+i' : ['fixed', 1.0],
 
     # # RV planetary
-    'K_p1' : ['uniform', [0.05,0.25]], # it is given in km/s
-
-    # RV FEROS
-    'mu_FEROS' : ['uniform', [-10,40]],
-    'sigma_w_FEROS' : ['loguniform', [1e-5,1.]],
+    # 'K_p1' : ['uniform', [0.05,0.25]], # it is given in km/s
+    #
+    # # RV FEROS
+    # 'mu_FEROS' : ['uniform', [-10,40]],
+    # 'sigma_w_FEROS' : ['loguniform', [1e-5,1.]],
 
     # RV CORALIE
-    'mu_CORALIE' : ['uniform', [-10,40]],
-    'sigma_w_CORALIE' : ['loguniform', [1e-5,1.]],
+    # 'mu_CORALIE' : ['uniform', [-10,40]],
+    # 'sigma_w_CORALIE' : ['loguniform', [1e-5,1.]],
 
     # long-term trend
     # 'rv_intercept' : ['normal', [0.0,100]],
@@ -174,7 +175,7 @@ def main(datafolder, out_folder, GP):
 
     dataset = juliet.load(
         priors=priors, t_lc=times_lc, y_lc=fluxes, yerr_lc=fluxes_error,
-        t_rv=times_rv, y_rv=rvs, yerr_rv=rvs_error,
+        # t_rv=times_rv, y_rv=rvs, yerr_rv=rvs_error,
         GP_regressors_lc=GP_regressors,
         out_folder=out_folder, verbose=True)
     results = dataset.fit(use_dynesty=True, n_live_points=500, ecclim=0.7,
@@ -182,52 +183,41 @@ def main(datafolder, out_folder, GP):
                           pl=pl, pu=pu)
     return
 
-def showResults(datafolder, out_folder, GP, **fitKwargs):
-    priors, params = get_priors(GP)
-    times_lc, fluxes, fluxes_error, gp_times_lc = read_photometry(datafolder,
-                                                    plotPhot=False, outlierIndices=outlierIndices)
-    times_rv, rvs, rvs_error = read_rv(datafolder)
-
-    if GP:
-        GP_regressors = gp_times_lc
-    else:
-        GP_regressors = None
-
-    dataset = juliet.load(
-        priors=priors, t_lc=times_lc, y_lc=fluxes, yerr_lc=fluxes_error,
-        # t_rv=times_rv, y_rv=rvs, yerr_rv=rvs_error,
-        GP_regressors_lc=GP_regressors,
-        out_folder=out_folder, verbose=True)
+def showResults(datafolder, out_folder, **fitKwargs):
+    dataset = juliet.load(input_folder=out_folder)
     results = dataset.fit(use_dynesty=True, dynamic=True,
                           **fitKwargs) # has to be ~same call as during fit
 
     # dianaplot.plot(dataset, results)
 
     # # plot posteriors
-    fig = plots.plot_cornerPlot(results, params, pl=results.pl, pu=results.pu,
+    fig = plots.plot_cornerPlot(results, pl=results.pl, pu=results.pu,
                                 quantiles=[0.16, 0.5, 0.84], show_titles=True,
                                 title_kwargs={"fontsize": 14}, title_fmt='.2f',
                                 rasterized=True,
-                                label_kwargs={"fontsize": 14}
-                                )
+                                label_kwargs={"fontsize": 14})
     fig.savefig(out_folder + '/cornerPosteriors.pdf')
 
+    # plot single posterior plots
     plots.plot_posteriors(results, out_folder)
 
-    # # Plot the photometry with fit:
-    fig, axs = plots.plot_photometry(dataset, results)
-    axs[0].legend(loc='lower left', ncol=99, bbox_to_anchor=(0., 1.),
-                  frameon=False, columnspacing=1.6)
-    fig.savefig(out_folder + '/photometryFitted.pdf')
+    # Plot the photometry with fit
+    if dataset.ninstruments_lc is not None:
+        fig, axs = plots.plot_photometry(dataset, results)
+        axs[0].legend(loc='lower left', ncol=99, bbox_to_anchor=(0., 1.),
+                      frameon=False, columnspacing=1.6)
+        fig.savefig(out_folder + '/photometryFitted.pdf')
 
-    # plot RVs with fit:
-    fig, ax = plots.plot_rv_fit(dataset, results)
-    fig.savefig(out_folder + '/RVsFitted.pdf')
+    # plot RVs with fit
+    if dataset.ninstruments_rv is not None:
+        fig, ax = plots.plot_rv_fit(dataset, results)
+        fig.savefig(out_folder + '/RVsFitted.pdf')
 
+    print(r'Log - evidence: {0: .3f} $\pm$ {1: .3f}'.format(results.posteriors['lnZ'],\
+                                                           results.posteriors['lnZerr']))
     return results
 
 if __name__ == "__main__":
-    # main(datafolder, out_folder, GP)
-    results = showResults(datafolder, out_folder, GP, pl=pl, pu=pu)
-    print(r'Log - evidence: {0: .3f} $\pm$ {1: .3f}'.format(results.posteriors['lnZ'],\
-                                                           results.posteriors['lnZerr']))
+    main(datafolder, out_folder, GP)
+    results = showResults(datafolder, out_folder, pl=pl, pu=pu)
+
