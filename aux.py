582ffb548909bdbee81c6484fa3042006794467d
Martin Schlecker <schlecker@mpia.de>
2020-04-13 16:54:26 +0200

    'BJD' as unit

--- aux.py
+++ aux.py
@@ -22,7 +22,7 @@ def format(paramName):
 
     labels = {
     'P_p1' : '$P \, [\mathrm{d}]$',
-    't0_p1' :'$t_0\,\mathrm{d}$',
+    't0_p1' :'$t_0\, [\mathrm{BJD}]$',
     'r1_p1' :'$r_1$',
     'r2_p1' :'$r_2$',
     'b_p1' : '$b$',
